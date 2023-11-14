c0b5efa55f1d0677e5fc5ea07a2f7b8da876e6b3
otisdev95 <olaliotis95@gmail.com>
2023-11-14 13:08:49 +0100

    updated  boiler-project-urlshortener

--- index.js
+++ index.js
@@ -19,6 +19,41 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// STEPS TO BUILD A URL SHORTENER
+// 1. PASS IN express.json MIDDLEWARE TO HANDLE urlEncoded
+//    app.use(express.urlEncoded({ extended: true }));
+// 2. DECLARE A POST END-POINT
+// 3. DECLARE originalUrls and shortUrls []
+// 4. DECLARE url
+// 5. DECLARE foundIndex OF originalUrls TO url
+// 6. DECLARE ERROR MSG FOR WRONG URL
+//    if (!url.includes('http://') && !url.includes('https://')) {
+//      return res.json({ error: 'invalid url' });
+//    }
+// 7. 
+// 8. PUSH originalUlrs TO url AND shortUrls TO shortUrls.length
+// 9. RETURN A JSON RES of original_url: to url AND short_url to - 1.
+//10. BUT IF FOUND, RETURN JSON RES OF original_url TO url AND
+//    short_url TO shortUrls[foundIndex]
+//11. DECLARE A GET END-POINT app.get('/api/shorturl/:shorturl')
+//12. DECLARE shorturl (parseInt TO TURN IT INTO A NUMBER)
+//13. DECLARE foundIndex OF shortUrls TO shorturl
+//14. DECLARE IF STATEMENT IF foundIndex IS NOT FOUND
+//15. RETURN JSON RES "error": "No short URL found for the given input"
+//16. redirect THE RES TO THE originalUrls foundIndex
+
+app.post('/api/shorturl', (req, res) => {
+  if (inValidUrl) {
+    res.json({ error: 'invalid url' });
+    return;
+  }
+
+  res.json({
+    original_url: 'https://freeCodeCamp.org',
+    short_url: 1
+  });
+});
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
